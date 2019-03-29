888c3c06f1825265c92c89d6e655b18b90816fb3
Mateus Malaquias <mateus.malaquias@globalhitss.com.br>
2019-03-29 17:46:04 -0300

    refactor: change some rules of eslint

--- index.js
+++ index.js
@@ -10,12 +10,26 @@ module.exports = {
     'max-statements-per-line': ['warn', {max: 1}],
     'max-nested-callbacks': ['warn', {max: 2}],
     'max-depth': ['warn', {max: 2}],
-    eqeqeq: ['warn', 'always'],
-    'no-return-assign': ['warn', 'always'],
     'max-lines': ['warn', {max: 50, skipComments: true}],
-    'max-len': ['warn', 80, {ignoreUrls: true}],
-    'no-param-reassign': 'warn',
+    'max-len': ['warn', 120, {ignoreUrls: true}],
     'max-params': ['warn', 3],
+    'no-param-reassign': [
+      'warn',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'acc', // for reduce accumulators
+          'accumulator', // for reduce accumulators
+          'item', // for map interator
+          'e', // for e.returnvalue
+          'module', // for curry functions
+          'req', // for requests obj
+          'request', // for requests obj
+          'res', // for responses obj
+          'response', // for response obj
+        ],
+      },
+    ],
   },
   globals: {
     describe: true,
